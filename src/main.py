66a31fd30607ec8118bca839993d8a2b9fbc10d5
jb4rr <james.barrett0002@gmail.com>
2022-01-31 14:15:37 +0000

    Added Model Foundations

--- src/main.py
+++ src/main.py
@@ -3,16 +3,17 @@
 # Date: 27/01/22
 
 import torch
+import config
 from torch.utils.data import DataLoader, sampler
-from utils.preprocessing import LUCASDataset
+from utils.dataloader import LUCASDataset
 
 def main():
     train_data = LUCASDataset('train_file.csv')
     test_data = LUCASDataset('test_file.csv')
     w_sampler = sampler.WeightedRandomSampler(train_data.weights, len(train_data.weights))
     print(w_sampler)
-    train_loader = DataLoader(train_data, sampler=w_sampler,batch_size=4)
-    test_loader = DataLoader(test_data, batch_size=4)
+    train_loader = DataLoader(train_data, sampler=w_sampler,batch_size=config.BATCH_SIZE)
+    test_loader = DataLoader(test_data, batch_size=config.BATCH_SIZE)
 
     for i_batch, sample_batched in enumerate(train_loader):
         images_batch, labels_batch = sample_batched['image'], sample_batched['label']
--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,96 +1,12 @@
-from sys import path
-path.append('utils/')
+# Title: preprocessing.py
+# Date: 31/01/227
+# Author: James Barrett
 
-import config
-import nibabel as nib
-import os
-import pandas as pd
-import torch
-import numpy as np
-from torch.utils.data import Dataset
+# Description: Adopts the preprocessing steps defined by Liao et al. (2019) for KDSB17
 
-
-class LUCASDataset(Dataset):
-    '''
-        Description: Custom DataLoader for LUCAS Dataset
-    '''
-    def __init__(self, csv_file, transform=None):
-        self.root = config.DATA_DIR
-        labels = pd.read_csv(os.path.join(self.root, csv_file))
-        self.labels = labels.set_index('patient_id').T.to_dict('list')
-        self.idx = self.idx = list(self.labels.keys())
-        self.task = -2 # Used to Index Column -2 = Cancer
-        self.image_size = np.asarray((256,256,256))
-        # Omitting removing samples with smaller size
-        self.weights = self.weights_balanced()
-        #self.transform = transform
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        patient = self.idx[idx]
-        print(self.labels)
-        label = self.labels[patient][self.task]
-        image_dir = os.path.join(self.root, "SCANS", str(patient) + ".nii.gz")
-        image = np.array(nib.load(image_dir).dataobj)
-
-        #if any(np.asarray(image.shape) <= self.image_size):
-        #    dif = self.image_size - image.shape
-        #    mod = dif % 2
-        #    dif = dif // 2
-        #    pad = np.maximum(dif, [0, 0, 0])
-        #    pad = tuple(zip(pad, pad + mod))
-        #    image = np.pad(image, pad, 'reflect')
-
-        #sz = self.image_size[0]
-        #if any(np.asarray(image.shape) >= self.image_size):
-        #    x, y, z = image.shape
-        #    x = x // 2 - (sz // 2)
-        #    y = y // 2 - (sz // 2)
-        #    z = z // 2 - (sz // 2)
-        #    image = image[x:x + sz, y:y + sz, z:z + sz]
-        # Stats obtained from the MSD dataset
-        image = np.clip(image, a_min=-1024, a_max=326)
-        image = (image - 159.14433291523548) / 323.0573880113456
-
-        return {'image': np.expand_dims(image, 0), 'label': label}
-
-    def weights_balanced(self):
-        count = [0] * 2
-        for item in self.idx:
-            count[self.labels[item][self.task]] += 1
-        weight_per_class = [0.] * 2
-        N = float(sum(count))
-        for i in range(2):
-            weight_per_class[i] = N/float(count[i])
-        weight = [0] * len(self.idx)
-        for idx, val in enumerate(self.idx):
-            weight[idx] = weight_per_class[self.labels[val][self.task]]
-        return weight
-
-    def transform(self):
-        pass
-
-
-def show_slices(slices):
-   """ Function to display row of image slices """
-   fig, axes = plt.subplots(1, len(slices))
-   for i, slice in enumerate(slices):
-       axes[i].imshow(slice.T, cmap="gray", origin="lower")
-
-if __name__ == '__main__':
-    dataset = LUCASDataset('train_file.csv')
-    epi_img_data = dataset[0]['image']
-    import matplotlib.pyplot as plt
-    print(epi_img_data.shape)
-    """ Function to display row of image slices """
-    slice_0 = epi_img_data[:,26, :, :]
-    slice_1 = epi_img_data[:,:, 30, :]
-    slice_2 = epi_img_data[:,:, :, 200]
-    show_slices([slice_0, slice_1, slice_2])
-    plt.suptitle("Center slices for EPI image")
-    plt.show()
+def main(slices):
+    """
+    :param slices: list of numpy.ndarray for each slice
+    :return: processed scan images as uint16
+    """
+    # Do model implemention with no preprocessing except done by DAZA et al. (2021)
