a0cfb40c580f25ed0f603f3923b6c9a8c45afff4
jb4rr <james.barrett0002@gmail.com>
2022-04-12 19:57:55 +0100

    Cont. Development

--- src/util/nets/Fusion.py
+++ src/util/nets/Fusion.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 import torch
 import src.config as config
-from .DAE import DenoisingAutoEncoder
-from .AlignedXception import AlignedXception
+from DAE import DenoisingAutoEncoder
+from AlignedXception import AlignedXception
 
 
 class Fusion(nn.Module):
@@ -16,9 +16,7 @@ class Fusion(nn.Module):
 
         # Load Pretrained Models
         self.DAE = DenoisingAutoEncoder()
-        self.DAE.load_state_dict(torch.load(dae_model)['state_dict'])
-
-        self.DAE.eval()  # Disable Dropout
+        self.load_DAE_model(dae_model)
 
         # Combination
         self._fc0 = nn.Linear(filters[-1] * 2 * 2 * 2 + 1480, filters[-1])  # 1480 where N = 20
@@ -27,7 +25,6 @@ class Fusion(nn.Module):
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x, y):
-
         # Images
         # ---------For Simple Fusion -----------#
         x = self.backbone(x)
@@ -43,9 +40,23 @@ class Fusion(nn.Module):
         x = self._fc(x)
         return x
 
+    def load_DAE_model(self, dae_model):
+        self.DAE.to(device=config.DEVICE)
+        self.DAE.float()
+        self.DAE.load_state_dict(torch.load(dae_model, map_location=torch.device(config.DEVICE))['state_dict'])
+        self.DAE.eval()  # Disable Dropout
+        for param in self.DAE.parameters():
+            param.requires_grad = False
+
 
 if __name__ == '__main__':
     model = Fusion()
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            print(f"Grad == True:  {name}")
+        else:
+            print(f"Grad == False: {param.data}")
+
     img_input, label_input = torch.rand((2, 1, 128, 128, 128)), torch.rand((2, 1, config.NUM_FEATURES))
     out = model(img_input, label_input)
     print(out.shape)
